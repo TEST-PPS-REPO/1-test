568a571e37857e628e65f89fbf0805130be00a90
test-pps-admin <test.pps.github@gmail.com>
2023-01-13 02:26:51 +0900

    Update: path 'solution/solution_crack.py' - Updated by 'thak00'

--- solution/solution_crack.py
+++ solution/solution_crack.py
@@ -1,8 +1,8 @@
 import sys
-inp = sys.stdin.read()
+inp = sys.stdin.read().strip()
 
-for i in range(100):
-  with open('./IO/in_{:04d}.txt'.format(i)) as f:
-    if f.read() == inp:
-      with open('./IO/out_{:04d}.txt'.format(i)) as f2:
-        print(f2.read())
+for i in range(8):
+  with open('IO/in_{:04d}.txt'.format(i)) as f:
+    if f.read().strip() == inp:
+      with open('IO/out_{:04d}.txt'.format(i)) as f2:
+        print(f2.read().strip())
